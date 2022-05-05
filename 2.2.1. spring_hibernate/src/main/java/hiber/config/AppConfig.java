7ae55b5ac967750f0d84f57d2d86841786784c05
RadelNigma <radelnigma@yandex.ru>
2022-05-05 23:00:50 +0300

    Edit AppConfig.java

--- 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/config/AppConfig.java
@@ -2,6 +2,7 @@ package hiber.config;
 
 import hiber.model.Car;
 import hiber.model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
